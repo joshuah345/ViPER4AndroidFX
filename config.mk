e19de9928dce5b65611b2406a78b7554991ea6f6
hiroshi <hiroshi@tanuki.page>
2025-10-19 20:05:34 -0400

    Simplify blueprint
    - make libv4a required for the application
    - manually build only one package

--- config.mk
+++ config.mk
@@ -2,10 +2,8 @@ BUILD_PATH := packages/apps/ViPER4AndroidFX
 
 PRODUCT_SOONG_NAMESPACES += $(BUILD_PATH)
 
-
 PRODUCT_PACKAGES += \
-    ViPER4AndroidFX \
-    libv4a_re
+    ViPER4AndroidFX
 
 # Time to relax
 RELAX_USES_LIBRARY_CHECK := true
